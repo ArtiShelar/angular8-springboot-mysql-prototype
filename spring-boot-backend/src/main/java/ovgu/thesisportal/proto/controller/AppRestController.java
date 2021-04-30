7c7fc61d3cee3f7d61372d3b0c6bdab4fc8e1617
Arti Shelar <arti.shelar@st.ovgu.de>
2021-04-30 18:36:44 +0200

    initializing spring-boot project

--- spring-boot-backend/src/main/java/ovgu/thesisportal/proto/controller/AppRestController.java
+++ spring-boot-backend/src/main/java/ovgu/thesisportal/proto/controller/AppRestController.java
@@ -2,6 +2,9 @@ package ovgu.thesisportal.proto.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.util.List;
+
 @RestController
 public class AppRestController {
 
@@ -10,4 +13,15 @@ public class AppRestController {
 		return "Hello, spring boot is working!";
 	}
 
+	@RequestMapping("/add")
+	public void add() throws IOException {
+	}
+
+	@RequestMapping("/update")
+	public void update() throws IOException {
+	}
+
+	@RequestMapping("/read")
+	public void read() throws IOException {
+	}
 }
